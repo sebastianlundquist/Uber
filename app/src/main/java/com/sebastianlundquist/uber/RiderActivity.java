4cbb49c9f5e85d021167e10aef7566fada751618
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-06 13:35:51 +0200

    Add functionality to send and cancel Uber requests to Parse

--- app/src/main/java/com/sebastianlundquist/uber/RiderActivity.java
+++ app/src/main/java/com/sebastianlundquist/uber/RiderActivity.java
@@ -13,6 +13,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +23,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class RiderActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -27,12 +39,60 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
 
 	LocationManager locationManager;
 	LocationListener locationListener;
+	Button requestUberButton;
+	Boolean requestActive = false;
 
 	public void updateMap(Location location) {
 		LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
 		mMap.clear();
 		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
 		mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+	}
+
+	public void requestUber(View view) {
+
+		if (requestActive) {
+			ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Request");
+			query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+			query.findInBackground(new FindCallback<ParseObject>() {
+				@Override
+				public void done(List<ParseObject> objects, ParseException e) {
+					if (e == null) {
+						if (objects.size() > 0) {
+							for (ParseObject object : objects) {
+								object.deleteInBackground();
+							}
+							requestActive = false;
+							requestUberButton.setText("Request \u040F\u0432\u0454\u0433");
+						}
+					}
+				}
+			});
+		}
+		else {
+			if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+				locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, locationListener);
+				Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+				if (lastKnownLocation != null) {
+					ParseObject request = new ParseObject("Request");
+					request.put("username", ParseUser.getCurrentUser().getUsername());
+					ParseGeoPoint parseGeoPoint = new ParseGeoPoint(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+					request.put("location", parseGeoPoint);
+					request.saveInBackground(new SaveCallback() {
+						@Override
+						public void done(ParseException e) {
+							if (e == null) {
+								requestActive = true;
+								requestUberButton.setText("Cancel \u040F\u0432\u0454\u0433");
+							}
+						}
+					});
+				}
+				else {
+					Toast.makeText(this, "Could not find location. Please try again later.", Toast.LENGTH_SHORT).show();
+				}
+			}
+		}
 	}
 
 	@Override
@@ -43,6 +103,21 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
+		requestUberButton = findViewById(R.id.requestUberButton);
+		requestUberButton.setText("Request \u040F\u0432\u0454\u0433");
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Request");
+		query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (e == null) {
+					if (objects.size() > 0) {
+						requestActive = true;
+						requestUberButton.setText("Cancel \u040F\u0432\u0454\u0433");
+					}
+				}
+			}
+		});
 	}
 
 	@Override
